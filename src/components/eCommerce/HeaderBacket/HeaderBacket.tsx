9303edffb571c9e827f31725792132c8189a7eaf
Abdullah <abdulhhany69@gmail.com>
2025-10-03 09:18:55 +0300

    show: show total quantity

--- src/components/eCommerce/HeaderBacket/HeaderBacket.tsx
+++ src/components/eCommerce/HeaderBacket/HeaderBacket.tsx
@@ -1,13 +1,20 @@
 import Logo from "@assets/svg/cart.svg?react";
 
+import { useAppSelector } from "@store/hooks";
 import styles from "./styles.module.css";
 const { basketContainer, basketQuantity } = styles;
 
 const HeaderBasket = () => {
+  const cartItems = useAppSelector((state) => state.cart.items);
+  const totalQuantity = Object.values(cartItems).reduce(
+    (acc, current) => acc + current,
+    0
+  );
+
   return (
     <div className={basketContainer}>
       <Logo title="basket icon" />
-      <div className={basketQuantity}>0</div>
+      <div className={basketQuantity}>{totalQuantity}</div>
     </div>
   );
 };
